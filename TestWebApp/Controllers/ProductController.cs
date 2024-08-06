92662f7681fe5ae3fb893531942c78ddcfc59fd6
Nguyễn Đại <dainh@kas.asia>
2024-08-06 21:58:23 +0700

    first commit

---
+++ TestWebApp/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
